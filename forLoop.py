7376a4ddf2c857dfff9cbfc06f6cc16aee0358d3
jk78346 <jk78346@gmail.com>
2018-09-11 16:11:29 -0400

    jupyter

--- forLoop.py
+++
@@ -1,9 +0,0 @@
-import dis
-def forLoop():
-	x = 0
-	for x in range(5):
-		x += 1
-	return x
-dis.dis(forLoop)
-
-
--- whileLoop.py
+++
@@ -1,8 +0,0 @@
-import dis
-def whileLoop():
-	x = 0
-	while x < 5:
-		x += 1
-	return
-dis.dis(whileLoop)
-
